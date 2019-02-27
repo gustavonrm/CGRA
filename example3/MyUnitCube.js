2d7015db8bc4fe0e77a360f8ef42af2ad0097674
gustavonrm <gustavonrm@gmail.com>
2019-02-27 18:16:09 +0000

    cube contructed

--- example3/MyUnitCube.js
+++ example3/MyUnitCube.js
@@ -23,24 +23,24 @@ class MyUnitCube extends CGFobject {
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
-            //down 
-            0, 1, 2,
-            0, 3, 2,
-            //up
+            //down             
+            0, 2, 1,            //why are there clockwise??
+            0, 3, 2,            //positive-> front, right, up
+            //up                //negative-> back, left, down 
             4,5,6,
             4,6,7,
             //right
             1,2,6,
             1,6,5,
             //left
-            0,3,7,
-            0,7,4,
+            0,7,3,
+            0,4,7,
             //front
             0,1,5, 
             0,5,4,
             //back
-            3,2,6,
-            3,6,7
+            3,6,2,
+            3,7,6
 		];
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
